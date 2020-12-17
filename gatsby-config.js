ed0057b218b3d681516e5a7996a58c7244d89002
tksat <satoko.takada@findy.co.jp>
2020-12-17 21:27:59 +0900

    ナビゲーションの追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,15 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+
 module.exports = {
-  /* Your site config here */
-  plugins: [`gatsby-plugin-sass`]
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
